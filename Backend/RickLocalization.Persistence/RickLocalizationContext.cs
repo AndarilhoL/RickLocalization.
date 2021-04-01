1572532fc62db290bf776049f44a4b72e51d6684
Leonardo Santos Silva <leonardosantos2742@gmail.com>
2021-04-01 19:25:44 -0300

    ajustando problemas de relacionamento

--- Backend/RickLocalization.Persistence/RickLocalizationContext.cs
+++ Backend/RickLocalization.Persistence/RickLocalizationContext.cs
@@ -16,7 +16,27 @@ namespace RickLocalization.Persistence
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
-              
+            builder.Entity<Rick>()
+                .HasOne(r => r.Morty)
+                .WithOne(m => m.Rick)
+                .HasForeignKey<Morty>(m => m.Id);
+
+            builder.Entity<Morty>()
+                .HasOne(m => m.Rick)
+                .WithOne(r => r.Morty)
+                .HasForeignKey<Rick>(r => r.Id);
+
+            //Dimensão -> Rick
+            builder.Entity<Dimension>()
+                .HasOne(d => d.Rick)
+                .WithOne(r => r.Dimension)
+                .HasForeignKey<Rick>(r => r.Id);
+
+            //Dimensão -> Morty
+            builder.Entity<Dimension>()
+                .HasOne(d => d.Morty)
+                .WithOne(m => m.Dimension)
+                .HasForeignKey<Morty>(m => m.Id);
         }
     }
 }
